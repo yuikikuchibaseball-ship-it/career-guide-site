37877650e7d72674b0e2e9ffb36b50e30aa4dd7d
Yuui Kikuchi <yuikikuchibaseball-ship-it@users.noreply.github.com>
2026-06-10 01:25:43 +0900

    Make site design bolder

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "career-guide-v14";
+const CACHE_NAME = "career-guide-v15";
 const CORE_ASSETS = ["./", "./index.html", "./job-sales.html", "./styles.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
